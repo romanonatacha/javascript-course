6815676f3ecb0f0e44b896dce0449b35eee3d46a
Natacha Romano <natacharomanonr@gmail.com>
2019-06-05 17:19:38 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var velo = 12
+console.log(`a velocidade so seu carro é ${velo}km/h`)
+
+if (velo > 60) { // condição simples
+    console.log('você foi multado')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
