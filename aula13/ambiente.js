3973b863f1b187c628e470ff1c792e6d43254b76
natacha romano <natacharomanonr@gmail.com>
2019-06-16 18:18:36 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var count = 1
+do {
+    console.log(`Passo ${count}`)
+    count++
+} while (count <= 10)
+
+// var count = 1
+// while (count <= 10) {
+//     console.log(`Passo ${count}`)
+//     count++
+// }
